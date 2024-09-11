c57096e27a5978e1fff61c80f5fcb0a1b187b312
M-Nassir <Nassir.M@live.co.uk>
2024-09-11 15:33:43 +0100

    created new script for public use

---
+++ scripts/point_detection/isolpix_detection.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 15:25:50 2024
+
+@author: nassirmohammad
+
+This script has been written to read or be supplied digital images that contain
+isolated pixels to be detected. A selection of images are provided, users can
+also upload their own images and run the perception isolpix detection method.
+"""
+
+# %%

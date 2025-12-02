4909ed05759e5d9a46ff64a89fdb9520d0fd0db7
M-Nassir <Nassir.M@live.co.uk>
2025-12-02 16:08:10 +0000

    some modifications for testing, kept additional function for future use

--- scripts/point_detection/process_image.py
+++ scripts/point_detection/process_image.py
@@ -89,8 +89,79 @@ def load_and_preprocess_image(img_name, data_path):
         binary_image_flag = True
         img_title = "Original image with 3 added isolated pixels"
 
+    elif img_name == 'ab1_cropped.png':
+        # Add isolated pixels
+        # img[200][100] = 240
+        # img[75][150] = 255
+        # img[300][100] = 255
+        # img[300][350] = 20
+        # img[150][150] = 255
+        # img[100][120] = 255
+        # img[250][350] = 0
+
+        binary_image_flag = False
+        img_title = 'Original image with 7 isolated pixels'
+
+    elif img_name == 'spatial_anomaly_nodes.png':
+        binary_image_flag = True
+        img_title = 'Original image with original synthetic isolated (10) pixels'
+
     else:
         binary_image_flag = False
         img_title = 'Original image'
 
     return img, binary_image_flag, img_title
+
+
+
+#  specify the greyscale images to input
+def add_isolated_pixels_to_image_if_required(img_array, img_name):
+    """
+    Adds isolated pixels to the image array depending on the image name.
+    Returns updated img_array and binary_image_flag (bool).
+    """
+
+    binary_image_flag = False  # Default
+
+    if img_name == 'ab1_cropped.png':
+        # Add isolated pixels
+        img_array[200][100] = 240
+        img_array[75][150] = 255
+        img_array[300][100] = 255
+        img_array[300][350] = 20
+        img_array[150][150] = 255
+        img_array[100][120] = 255
+        img_array[250][350] = 0
+
+        print('Number of isolated pixels added: 7')
+        binary_image_flag = False
+
+    elif img_name == 'turbine_blade_black_dot.png':
+        binary_image_flag = False
+
+    elif img_name == 'camera_man.png':
+        # Add isolated pixels
+        img_array[200][100] = 255
+        img_array[75][150] = 255
+        img_array[200][140] = 50
+        img_array[250][150] = 255
+        img_array[150][150] = 150
+        img_array[100][120] = 90
+
+        print('Number of isolated pixels added: 6')
+        binary_image_flag = False
+
+    elif img_name == 'spatial_anomaly_nodes.png':
+        binary_image_flag = True
+
+    elif img_name == 'test13.png':
+        binary_image_flag = False
+
+    else:
+        print(f"No isolated pixels added for image: {img_name}")
+        binary_image_flag = False
+
+    # Ensure values are clipped between 0 and 255
+    img_array = np.clip(img_array, 0, 255).astype(np.uint8)
+
+    return img_array, binary_image_flag
